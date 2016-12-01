4ced7f01c4fbbae416f85d314c9a761dcbf3ce93
eidanlin <wenan_39141@163.com>
2016-12-01 22:05:43 +0800

    美化 页面

--- MapLabelDemo/ViewController.m
+++ MapLabelDemo/ViewController.m
@@ -12,7 +12,10 @@
 
 @interface ViewController ()<MAMapViewDelegate>
 
-@property (nonatomic, strong) MAMapView *mapView;
+@property (strong, nonatomic) MAMapView *mapView;
+
+//xib view
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 
 @end
 
@@ -22,19 +25,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    self.containerView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.4];
+    self.containerView.layer.cornerRadius = 10;
+    
     self.mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
     self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     self.mapView.delegate = self;
 
     [self.view addSubview:self.mapView];
+    [self.view sendSubviewToBack:self.mapView];
     
-    
-    UIView *switchsPannelView = [self makeSwitchsPannelView];
-    switchsPannelView.center = CGPointMake( CGRectGetMidX(switchsPannelView.bounds) + 10,
-                                           self.view.bounds.size.height -  CGRectGetMidY(switchsPannelView.bounds) - 20);
-    
-    switchsPannelView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin;
-    [self.view addSubview:switchsPannelView];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -52,44 +52,13 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (UIView *)makeSwitchsPannelView
-{
-    UIView *ret = [[UIView alloc] initWithFrame:CGRectZero];
-    ret.backgroundColor = [UIColor whiteColor];
-    
-    
-    UISwitch *swt1 = [[UISwitch alloc] init];
-    
-    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 70, CGRectGetHeight(swt1.bounds))];
-    label1.text = @"showsLabels:";
-    [label1 sizeToFit];
-    
-    
-    [ret addSubview:label1];
-    [ret addSubview:swt1];
-    
-    // layout
-    CGRect tempFrame = swt1.frame;
-    tempFrame.origin.x = CGRectGetMaxX(label1.frame) + 5;
-    swt1.frame = tempFrame;
-    
-    
-    //
-    [swt1 addTarget:self action:@selector(actionLabel:) forControlEvents:UIControlEventValueChanged];
-    
-    [swt1 setOn:self.mapView.showsLabels];
-    
-    ret.bounds = CGRectMake(0, 0, CGRectGetMaxX(swt1.frame), CGRectGetMaxY(swt1.frame));
-    
-    return ret;
-}
-
 #pragma - action
 
-- (void)actionLabel:(UISwitch *)sender
-{
-    self.mapView.showsLabels = sender.on;
+- (IBAction)showLabelSwitch:(id)sender {
+    UISwitch *showSwitch = (UISwitch *)sender;
+    self.mapView.showsLabels = showSwitch.on;
 }
+
 
 
 @end
